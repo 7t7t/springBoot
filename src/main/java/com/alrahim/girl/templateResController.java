1a75ae69d6055cd31cddea9b806064f3b954a52d
7t7t <395709574@qq.com>
2018-09-23 10:54:13 +0800

    add_template_wizard

---
+++ src/main/java/com/alrahim/girl/templateResController.java
@@ -0,0 +1,27 @@
+package com.alrahim.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+@EnableAutoConfiguration
+
+public class templateResController {
+    @Autowired
+    private GirlProties girlProties; // error here because have no @Component or not yet add dependency
+
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String say() {
+       return "index";
+    }
+}
+
